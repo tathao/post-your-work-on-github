f011c90e14066c807a5b45a0ba454ce854a45fa0
HaoN <haon@sysmex.com>
2023-08-14 16:10:11 +0700

    refractor: Added a print statement in the end of the code

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -232,7 +232,6 @@ def main():
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
-
-
+    print("\nDone\n")
 if __name__ == "__main__":
     main()
